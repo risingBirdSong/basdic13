0b7e7de1bf2e5201405c838e2e847968ad9b905c
Peter Murphy <naughtnowwhen@gmail.com>
2019-07-28 12:32:41 -0700

    made some good progress this morning, the basic concept is there, with some bugs, the most obvious is the styling gets all messed up when you click resume

--- code.js
+++ code.js
@@ -5,25 +5,32 @@ var htmlItem;
     htmlItem["h3"] = "h3";
 })(htmlItem || (htmlItem = {}));
 var isPaused = false;
-function appending(numbers, delayInMS, tag, isPaused) {
-    if (isPaused === void 0) { isPaused = false; }
+var pausePoint;
+var snapshot;
+var argument = 30;
+var howMuchToDelay = 100;
+function appending(numbers, delayInMS, tag, i, stateCondition) {
+    if (i === void 0) { i = 0; }
+    if (stateCondition === void 0) { stateCondition = "<p class='left_bracket child'>let outPutArr = [</p>"; }
+    console.log(delayInMS);
     var main = $(".main");
     var arrStart = $(".arr_start");
     var arrEnd = $(".arr_end");
     var insideArr = $(".inside_of_array");
-    arrStart.append("<p class='left_bracket child'>let outPutArr = [</p>");
-    var i = 0;
+    arrStart.append(stateCondition);
     function lognumbers(input) {
-        console.log(i);
-        if (isPaused === true) {
-            console.log('i should be paused');
-            delayInMS = 100000000;
-        }
+        console.log(argument);
         var myInterval = setInterval(function () {
             if (i >= input) {
-                console.log("i should clear");
                 clearInterval(myInterval);
             }
+            if (isPaused === true) {
+                clearInterval(myInterval);
+            }
+            pausePoint = i;
+            snapshot = $(".arr_start")
+                .html()
+                .concat($(".inside_of_array").html());
             var appendMe = "<" + tag + " id=\"current\">" + i + ", </" + tag + ">";
             insideArr.append(appendMe);
             setTimeout(function () {
@@ -37,10 +44,40 @@ function appending(numbers, delayInMS, tag, isPaused) {
 }
 function startTheSequence() {
     //@ts-ignore
-    appending(30, 300, htmlItem.p);
+    appending(argument, howMuchToDelay, htmlItem.p);
 }
 function pauseTheSequence() {
+    console.log("hit pause");
     isPaused = true;
+    console.log(snapshot);
+}
+function resumeSequence() {
+    isPaused = false;
+    $(".main div").empty();
+    appending(argument, howMuchToDelay, htmlItem.p, pausePoint, snapshot);
 }
 $(".begin").click(startTheSequence);
-$(".end").click(pauseTheSequence);
+$(".pause").click(pauseTheSequence);
+$(".resume").click(resumeSequence);
+$(".argument").text(argument);
+$(".howManyNumbers").submit(function () {
+    argument = parseInt(
+    //@ts-ignore
+    $(this)
+        .children()
+        .val());
+    $(".argument").text(argument);
+});
+$(".howFast").submit(function () {
+    howMuchToDelay = parseFloat(
+    //@ts-ignore
+    $(this).children().val());
+});
+var outPutArr = [];
+function pushToArray(input) {
+    for (var i = 0; i <= input; i++) {
+        outPutArr.push(i);
+    }
+    return outPutArr;
+}
+pushToArray(argument);
--- code.ts
+++ code.ts
@@ -4,27 +4,40 @@ enum htmlItem {
 }
 
 let isPaused = false;
+let pausePoint: number;
+let snapshot: any;
+let argument: number = 30;
+let howMuchToDelay = 100;
 
-function appending(numbers: number, delayInMS: number, tag: htmlItem, isPaused = false) {
+function appending(
+  numbers: number,
+  delayInMS: number,
+  tag: htmlItem,
+  i: number = 0,
+  stateCondition: any = "<p class='left_bracket child'>let outPutArr = [</p>"
+) {
+  console.log(delayInMS);
   let main = $(".main");
   let arrStart = $(".arr_start");
   let arrEnd = $(".arr_end");
   let insideArr = $(".inside_of_array");
 
-  arrStart.append("<p class='left_bracket child'>let outPutArr = [</p>");
+  arrStart.append(stateCondition);
 
-  let i = 0;
   function lognumbers(input: number) {
-    console.log(i);
-    if (isPaused === true) {
-      console.log('i should be paused');
-      delayInMS = 100000000;
-    }
+    console.log(argument);
     let myInterval = setInterval(function() {
       if (i >= input) {
-        console.log("i should clear");
         clearInterval(myInterval);
       }
+      if (isPaused === true) {
+        clearInterval(myInterval);
+      }
+      pausePoint = i;
+      snapshot = $(".arr_start")
+        .html()
+        .concat($(".inside_of_array").html());
+
       let appendMe = `<${tag} id="current">${i}, </${tag}>`;
       insideArr.append(appendMe);
 
@@ -42,13 +55,50 @@ function appending(numbers: number, delayInMS: number, tag: htmlItem, isPaused =
 
 function startTheSequence() {
   //@ts-ignore
-  appending(30, 300, htmlItem.p);
+  appending(argument, howMuchToDelay, htmlItem.p);
 }
 
-function pauseTheSequence () {
+function pauseTheSequence() {
+  console.log("hit pause");
   isPaused = true;
+  console.log(snapshot);
+}
+
+function resumeSequence() {
+  isPaused = false;
+  $(".main div").empty();
+  appending(argument, howMuchToDelay, htmlItem.p, pausePoint, snapshot);
 }
 
 $(".begin").click(startTheSequence);
 
-$(".end").click(pauseTheSequence);
+$(".pause").click(pauseTheSequence);
+
+$(".resume").click(resumeSequence);
+
+$(".argument").text(argument);
+
+$(".howManyNumbers").submit(function() {
+  argument = parseInt(
+    //@ts-ignore
+    $(this)
+      .children()
+      .val()
+  );
+  $(".argument").text(argument);
+});
+
+$(".howFast").submit(function(){
+  howMuchToDelay = parseFloat(
+    //@ts-ignore
+    $(this).children().val());
+})
+
+let outPutArr: number[] = [];
+function pushToArray(input: number) {
+  for (let i = 0; i <= input; i++) {
+    outPutArr.push(i);
+  }
+  return outPutArr;
+}
+pushToArray(argument);
